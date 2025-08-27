d897e2753df7c56446b0cd5d5362f8593c8bb763
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-08-27 15:10:05 +0200

    chore: add flake.nix and .envrc

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          elixir
+          elixir-ls
+          typst
+          tinymist
+        ];
+      };
+    }
+  );
+}
