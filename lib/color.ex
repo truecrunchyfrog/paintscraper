f662a3b83c7214801f81508e3f752d31e8e1fbcd
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-09-06 23:12:15 +0200

    feat: Color.unique_id/1

--- lib/color.ex
+++ lib/color.ex
@@ -9,4 +9,9 @@ defmodule Paintscraper.Color do
     :url,
     :image_urls
   ]
+
+  def unique_id(%{vendor_color_code: vendor_color_code, vendor: vendor})
+    when is_binary(vendor_color_code) and is_atom(vendor) do
+    "#{vendor.vendor_name()} #{vendor_color_code}"
+  end
 end
